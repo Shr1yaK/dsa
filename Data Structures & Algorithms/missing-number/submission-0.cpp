39f5a0b0894a4f718febb4a17627c1dcc550c75f
Shr1yaK <shriya.kansal@students.iiit.ac.in>
2026-07-01 02:53:02 +0400

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) 
+    {
+        sort(nums.begin(), nums.end());
+
+        int curr = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (curr != nums[i])
+                return curr;
+
+            curr++;
+        }
+
+        return curr;
+    }
+};
