618bb9fcbaa57912e62a702ca00d576546f52655
Shr1yaK <shriya.kansal@students.iiit.ac.in>
2026-07-21 04:29:50 +0400

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool dfs(int node, vector<vector<int>>& graph, vector<bool>& visited, vector<bool>& pathVisited)
+    {
+        visited[node] = true;
+        pathVisited[node] = true;
+
+        for (int neighbor : graph[node])
+        {
+            if (!visited[neighbor])
+            {
+                if (dfs(neighbor, graph, visited, pathVisited))
+                    return true;
+            }
+
+            else if (pathVisited[neighbor])
+                return true;
+        }
+        pathVisited[node] = false;
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) 
+    {
+        vector<vector<int>> graph(numCourses);
+        for (auto edge : prerequisites)
+        {
+            int course = edge[0];
+            int prereq = edge[1];
+
+            graph[prereq].push_back(course);
+        }
+        
+        vector<bool> visited(numCourses, false);
+        vector<bool> pathVisited(numCourses, false);
+
+        for (int i = 0; i < numCourses; i++)
+        {
+            if (!visited[i])
+            {
+                if (dfs(i, graph, visited, pathVisited))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+};
